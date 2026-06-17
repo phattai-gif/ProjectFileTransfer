fd9c17bce85548ddfcc22055b34025095a604b76
hungtq8399-ship-it <hungtq8399@ut.edu.vn>
2026-06-18 03:42:55 +0700

    Hoan thanh UI  Client Connect Disconnect

--- ProjectFileTransferClient/Network/ClientManager.cs
+++ ProjectFileTransferClient/Network/ClientManager.cs
@@ -1,10 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net.Sockets;
 
 namespace ProjectFileTransferClient.Network
 {
-    internal class ClientManager
+    public class ClientManager
     {
+        private TcpClient client;
+
+        public bool Connect(string ip, int port)
+        {
+            try
+            {
+                client = new TcpClient();
+                client.Connect(ip, port);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (client != null)
+            {
+                client.Close();
+            }
+        }
+
+        public bool IsConnected()
+        {
+            return client != null && client.Connected;
+        }
     }
 }
